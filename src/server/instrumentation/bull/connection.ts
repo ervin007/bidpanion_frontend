359b1e17d715e8e4a94efc0a3270508abf3c9216
Miguel Verissimo <m.verissimo@gmail.com>
2025-04-28 06:11:58 +0100

    Add optional redis connection parameters

--- src/server/instrumentation/bull/connection.ts
+++ src/server/instrumentation/bull/connection.ts
@@ -1,11 +1,26 @@
 import { Redis } from "ioredis";
 import { serverEnv } from "@/env";
 
-export const bullConnection = new Redis({
+// Build connection options with required fields
+const connectionOptions: Record<string, any> = {
   host: serverEnv.REDIS_HOST,
   port: serverEnv.REDIS_PORT,
   maxRetriesPerRequest: null,
-});
+};
+
+if (serverEnv.REDIS_USERNAME) {
+  connectionOptions.username = serverEnv.REDIS_USERNAME;
+}
+
+if (serverEnv.REDIS_PASSWORD) {
+  connectionOptions.password = serverEnv.REDIS_PASSWORD;
+}
+
+if (serverEnv.REDIS_TLS) {
+  connectionOptions.tls = serverEnv.REDIS_TLS;
+}
+
+export const bullConnection = new Redis(connectionOptions);
 
 bullConnection.on("connect", () => {
   console.log("🐂 BullMQ connected to Redis");
